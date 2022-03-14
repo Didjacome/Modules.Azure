c6c0e5b91acbe1930197bcab1075aaa260a493ad
Diogo de Santana Jacome <diogo.jacome@solonetwork.com.br>
2022-03-14 19:13:21 -0300

    ajuste de module

--- module/Az.Adm.psm1
+++ module/Az.Adm.psm1
@@ -31,7 +31,7 @@
     .EXAMPLE
       C:\PS> Get-AzADGroupRBAC -Import .\AZAD_Groups.csv | export-csv Export-Csv C:\Users\$env:USERNAME\Documents\GroupUserAll.csv
 		.LINK 
-      https://github.com/Didjacome/GET-RBAC
+      https://github.com/Didjacome
 
 	
         
@@ -129,5 +129,117 @@ function  Get-AzADGroupRBAC {
 
 
 
- 
+<#
+ 	.SYNOPSIS
+      #################################################################################################################
+      #                              Criador: Diogo De Santana Jacome                                                 #
+      #                              Empresa:  Solo Network                                                           #
+      #                              Modifcado por: Diogo De Santana Jacome                                           #
+      #                                                                                                               #
+      #                                                                                                               #
+      #                                          Versão: 1.0                                                          #
+      #                                                                                                               #
+      #                                                                                                               #
+      #################################################################################################################   
+      Set-AzTag is an advanced function that can be used to configure tags on all resources in a resource group
+    
+    .DESCRIPTION
+      Set-AzTag is an advanced function that can be used to configure tags on all resources in a resource group
+
+      You need to be signed in to the Azure subscription
+
+      You need to have permission on the Azure subscription
+    
+    .EXAMPLE
+      C:\PS> Set-AzTag -RG "My Resource Group Name" -ImportCSV .\Tags.csv
+				
+    .EXAMPLE
+      C:\PS> Set-AzTag -RG "My Resource Group Name" -Tag_Name Env -Tag_value PRD
+    
+
+		.LINK 
+      https://github.com/Didjacome
+
+	
+        
+#>
+
+function Set-AzTag {
+
+
+  [CmdletBinding(DefaultParameterSetName = 'TAG')]
+
+  param (
+    # Parameter help description
+    [Parameter(Mandatory)]
+    [System.String]
+    $RG,
+
+    [Parameter(Mandatory, ParameterSetName = 'TAG')]
+    [System.String]
+    $Tag_Name,
+    
+    [Parameter(Mandatory, ParameterSetName = 'TAG')]
+    [System.String]
+    $Tag_Value,
+
+    [Parameter(Position = 0,
+      Mandatory, ValueFromPipelineByPropertyName,
+      ValueFromPipeline, ParameterSetName = 'CSV')]
+    [System.String]
+    $ImportCSV
+
+
+  )
+
+  process {
+    
+
+    Write-Verbose "ParameterSetName is '$($PSCmdlet.ParameterSetName)'"
+
+    if ($PSCmdlet.ParameterSetName -eq 'TAG') {
+
+      $tags = @{$Tag_Name = $Tag_Value }
+
+      $rg_id = (Get-AzResourceGroup -Name $RG).ResourceId
+  
+      $all_resource_id = (Get-AzResource -ResourceGroupName $RG).ResourceId
+  
+      foreach ( $resource in $all_resource_id ) {
+  
+        Update-AzTag -ResourceId $rg_id -Tag $tags -Operation Merge
+    
+        Update-AzTag -ResourceId $resource -Tag $tags  -Operation Merge
+      }
+    }
+
+    if ($PSCmdlet.ParameterSetName -eq 'CSV') {
+
+      function convertCsvToHashTable($ImportCSV) {
+        $csv = Import-Csv $ImportCSV -UseCulture
+        $headers = $csv[0].psobject.properties.name
+        $key = $headers[0]
+        $value = $headers[1]
+        $hashTable = @{}
+        $csv | ForEach-Object { $hashTable[$_."$key"] = $_."$value" }
+        return $hashTable
+      }
+
+      $tags = convertCsvToHashTable $ImportCSV
+
+      $rg_id = (Get-AzResourceGroup -Name $RG).ResourceId
+
+      $all_resource_id = (Get-AzResource -ResourceGroupName $RG).ResourceId
+
+      foreach ( $resource in $all_resource_id ) {
+  
+        Update-AzTag -ResourceId $rg_id -Tag $tags -Operation Merge
+      
+        Update-AzTag -ResourceId $resource -Tag $tags  -Operation Merge
+      }
+    }
+  
+  }   
+}
+
  
