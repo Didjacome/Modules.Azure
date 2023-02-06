22f1065c03327261d26bff3926df03b935e3afc0
Djacome <diogo.jacome@org.edu.br>
2023-02-06 02:33:14 -0300

    path validation

--- powershellgallery/publish.ps1
+++ powershellgallery/publish.ps1
@@ -1,3 +1,5 @@
 Get-Module -ListAvailable
+Install-Module PowerShellGet -MinimumVersion "3.0.11-beta" -AllowPrerelease -Repository PSGallery -Force
+Get-Module -ListAvailable
 Import-Module PowerShellGet
 Publish-PSResource  -Path $Env:PATH -Repository PSGallery -apikey $Env:APIKEY
