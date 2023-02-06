9b8dc8229a5706dacb2b7f9c5ed8bba42d84f59c
Djacome <diogo.jacome@org.edu.br>
2023-02-06 02:38:40 -0300

    path validation

--- powershellgallery/publish.ps1
+++ powershellgallery/publish.ps1
@@ -1,3 +1,4 @@
 Install-Module PowerShellGet -MinimumVersion "3.0.11-beta" -AllowPrerelease -Repository PSGallery -Force
 Import-Module PowerShellGet
-Publish-PSResource  -Path $Env:PATH/Az.Adm -Repository PSGallery -apikey $Env:APIKEY
+ls $Env:AZ
+Publish-PSResource  -Path $Env:AZ/Az.Adm -Repository PSGallery -apikey $Env:APIKEY
