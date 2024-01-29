966e1c4eee28469589588bf14e95e877b01491e6
Diogo De Santana Jacome <dsjacome@stefanini.com>
2024-01-29 02:17:04 -0300

    PowerShellGet 3.0.11

--- powershellgallery/publish.ps1
+++ powershellgallery/publish.ps1
@@ -1,4 +1,4 @@
 Install-Module PowerShellGet -RequiredVersion "3.0.11-beta" -AllowPrerelease -Repository PSGallery -Force
-Import-Module PowerShellGet -RequiredVersion "3.0.11-beta"
+Import-Module PowerShellGet -RequiredVersion "3.0.11"
 ls $Env:AZ
 Publish-PSResource  -Path $Env:AZ/Az.Adm -Repository PSGallery -apikey $Env:APIKEY
